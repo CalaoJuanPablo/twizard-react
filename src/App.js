82e9133da8858b37096e6cc94ee0cd96e2ca4350
Juan Pablo Calao Perez <juan.calao@ideaware.co>
2019-06-18 11:10:16 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App(props) {
+  return (
+    <React.Fragment>
+      <h1>I am in the app</h1>
+    </React.Fragment>
+  )
+}
+
+export default App
